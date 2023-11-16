45c9d3521992bce8f77904794e74315e729d6984
pap3rhat <hschweizer@techfak.uni-bielefeld.de>
2023-11-16 12:49:28 +0100

    Add settings and credits

---
+++ Assets/Scripts/Management/MainMenu.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _settings;
+    [SerializeField] private TMP_Dropdown _srSetting;
+    [SerializeField] private TMP_Dropdown _dmSetting;
+    [SerializeField] private Slider _usSetting;
+    [SerializeField] private TextMeshProUGUI _usSettingText;
+    [SerializeField] private Slider _mvSetting;
+    [SerializeField] private TextMeshProUGUI _mvSettingText;
+    [SerializeField] private Slider _svSetting;
+    [SerializeField] private TextMeshProUGUI _svSettingText;
+
+    [SerializeField] private GameObject _credits;
+
+    private void Start()
+    {
+        _settings.SetActive(false);
+
+        _srSetting.options = Screen.resolutions.Select(r => new TMP_Dropdown.OptionData(r.ToString())).ToList();
+
+        _dmSetting.options = FullScreenMode.GetNames(typeof(FullScreenMode)).ToList().Select(r => new TMP_Dropdown.OptionData(r.ToString())).ToList();
+
+        _usSetting.minValue = 0.25f;
+        _usSetting.maxValue = 5;
+        _usSetting.onValueChanged.AddListener(value => _usSettingText.text = value.ToString());
+        _usSetting.value = 1;
+
+        _mvSetting.minValue = 0;
+        _mvSetting.maxValue = 100;
+        _mvSetting.wholeNumbers = true;
+        _mvSetting.onValueChanged.AddListener(value => _mvSettingText.text = value.ToString());
+        _mvSetting.value = 50;
+
+
+        _svSetting.minValue = 0;
+        _svSetting.maxValue = 100;
+        _svSetting.wholeNumbers = true;
+        _svSetting.onValueChanged.AddListener(value => _svSettingText.text = value.ToString());
+        _svSetting.value = 50;
+
+        _credits.SetActive(false);
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void AccessSettings()
+    {
+        _settings.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        _settings.SetActive(false);
+    }
+
+    public void AccessCredits()
+    {
+        _credits.SetActive(true);
+
+    }
+
+    public void CloseCredits()
+    {
+        _credits.SetActive(false);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
