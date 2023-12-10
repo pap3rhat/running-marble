af2b6247ca1a0e32d18bca52b756674b95c52531
pap3rhat <hschweizer@techfak.uni-bielefeld.de>
2023-12-10 17:56:47 +0100

    Add menu fade

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -7,6 +8,7 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _settings;
+    private CanvasGroup _settingsCanvas;
     [SerializeField] private TMP_Dropdown _srSetting;
     [SerializeField] private TMP_Dropdown _dmSetting;
     [SerializeField] private Slider _usSetting;
@@ -17,10 +19,21 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _svSettingText;
 
     [SerializeField] private GameObject _credits;
+    private CanvasGroup _creditsCanvas;
+
+    private float _fadeInTime = 0.7f;
+    private float _fadeOutTime = 0.7f;
+
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
     private void Start()
     {
+        _settingsCanvas = _settings.GetComponent<CanvasGroup>();
+        _settingsCanvas.alpha = 0;
         _settings.SetActive(false);
+        _creditsCanvas = _credits.GetComponent<CanvasGroup>();
+        _creditsCanvas.alpha = 0;
+        _credits.SetActive(false);
 
         _srSetting.options = Screen.resolutions.Select(r => new TMP_Dropdown.OptionData(r.ToString())).ToList();
 
@@ -43,36 +56,67 @@ public class MainMenu : MonoBehaviour
         _svSetting.wholeNumbers = true;
         _svSetting.onValueChanged.AddListener(value => _svSettingText.text = value.ToString());
         _svSetting.value = 50;
-
-        _credits.SetActive(false);
     }
 
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+    /* START */
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
 
+    /* SETTINGS PANEL */
     public void AccessSettings()
     {
         _settings.SetActive(true);
+        StartCoroutine(FadeIn(_settingsCanvas));
     }
 
     public void CloseSettings()
     {
-        _settings.SetActive(false);
+        StartCoroutine(FadeOut(_settingsCanvas, _settings));
     }
 
+    /* CREDITS PANEL */
     public void AccessCredits()
     {
         _credits.SetActive(true);
-
+        StartCoroutine(FadeIn(_creditsCanvas));
     }
 
     public void CloseCredits()
     {
-        _credits.SetActive(false);
+        StartCoroutine(FadeOut(_creditsCanvas, _credits));
     }
 
+    /* FADE FUNCTIONS */
+    private IEnumerator FadeIn(CanvasGroup canvas)
+    {
+        float t = 0;
+        while (t < _fadeInTime)
+        {
+            t += Time.deltaTime;
+            canvas.alpha = t * 1/ _fadeInTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOut(CanvasGroup canvas, GameObject obj)
+    {
+        float t = 0;
+        while (t < _fadeOutTime)
+        {
+            t += Time.deltaTime;
+            canvas.alpha = 1 - (t * 1/ _fadeOutTime);
+            yield return null;
+        }
+
+        obj.SetActive(false);
+    }
+
+
+    /* QUIT */
     public void Quit()
     {
 #if UNITY_EDITOR
